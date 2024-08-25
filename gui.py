a4b9617a83303679c9e06c7edad06dedd3119a25
amiogithub <114328020+amiogithub@users.noreply.github.com>
2024-08-26 02:23:09 +0600

    Added edit and add case to the program

--- gui.py
+++ gui.py
@@ -5,9 +5,15 @@ import PySimpleGUI as me
 label=me.Text("Type in a to-do")
 input_box=me.InputText(tooltip="Enter todo",key='todo')
 add_button=me.Button("Add")
+list_box=me.Listbox(values=functions.get_todos(), key='todos',enable_events=True,
+                    size=[45,10])
+
+edit_button=me.Button("Edit")
+
+
 
 window=me.Window("My To-Do App",layout=[[label],
-[input_box, add_button]],
+[input_box, add_button],[list_box,edit_button]],
 font=('Helvetica',15))
 
 
@@ -21,9 +27,40 @@ while True:
             new_todo=values['todo']+ "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+             todo_to_edit=values['todos'][0]
+             new_todo=values['todo']
+
+             todos=functions.get_todos()
+             index=todos.index(todo_to_edit)
+             todos[index]=new_todo
+             functions.write_todos(todos)
+             window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
+
+
+
+
+
+
+
+
+
+
 
         case me.WINDOW_CLOSED:
             break
+
+
+
+
+
+
 
 
 
