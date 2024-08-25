edb7367e461960f72c49c37a185e19a88e57e349
amiogithub <114328020+amiogithub@users.noreply.github.com>
2024-08-26 02:47:41 +0600

    Implemented the Complete and Edit case

--- gui.py
+++ gui.py
@@ -10,10 +10,13 @@ list_box=me.Listbox(values=functions.get_todos(), key='todos',enable_events=True
 
 edit_button=me.Button("Edit")
 
+complete_button=me.Button("Complete")
 
+exit_button=me.Button("Exit")
 
 window=me.Window("My To-Do App",layout=[[label],
-[input_box, add_button],[list_box,edit_button]],
+[input_box, add_button],[list_box,edit_button,complete_button],
+[exit_button]],
 font=('Helvetica',15))
 
 
@@ -38,6 +41,19 @@ while True:
              todos[index]=new_todo
              functions.write_todos(todos)
              window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete=values['todos'][0]
+            todos=functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case "Exit":
+            break
+
+
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
