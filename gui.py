2af29361329cc7b5b59b83a92eba6fd2ffc0eff5
amiogithub <114328020+amiogithub@users.noreply.github.com>
2024-08-26 00:26:15 +0600

    Added a new feature "Add case"

--- gui.py
+++ gui.py
@@ -3,12 +3,30 @@ import functions
 import PySimpleGUI as me
 
 label=me.Text("Type in a to-do")
-input_box=me.InputText(tooltip="Enter todo")
+input_box=me.InputText(tooltip="Enter todo",key='todo')
 add_button=me.Button("Add")
 
-window=me.Window("My To-Do App",layout=[[label], [input_box, add_button]])
+window=me.Window("My To-Do App",layout=[[label],
+[input_box, add_button]],
+font=('Helvetica',15))
 
-window.read()
+
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values['todo']+ "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case me.WINDOW_CLOSED:
+            break
+
+
+
 window.close()
 
 
